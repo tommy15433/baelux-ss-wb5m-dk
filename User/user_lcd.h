9cd1cd2c8b45b7713a9f3fb809dc2fae29030658
Bosun <tommy15433@gmail.com>
2024-05-23 13:57:02 +0900

    lcd gpio control macro moved to driver

--- User/user_lcd.h
+++ User/user_lcd.h
@@ -35,50 +35,7 @@ extern "C"
 #define LCD_COLOR_BLACK SSD1315_COLOR_BLACK
 #define LCD_COLOR_WHITE SSD1315_COLOR_WHITE
 
-    /*##################### LCD ###################################*/
-    /**
-     * @brief  LCD Chip Select macro definition
-     */
-#define LCD_CS_LOW() HAL_GPIO_WritePin(LCD_CS_GPIO_PORT, LCD_CS_PIN, GPIO_PIN_RESET)
-#define LCD_CS_HIGH() HAL_GPIO_WritePin(LCD_CS_GPIO_PORT, LCD_CS_PIN, GPIO_PIN_SET)
-
-/**
- * @brief  LCD Control pins
- */
-#define LCD_CS_PIN GPIO_PIN_0  /* PH. 0*/
-#define LCD_CS_GPIO_PORT GPIOH /* GPIOH */
-#define LCD_CS_GPIO_CLK_ENABLE() __HAL_RCC_GPIOH_CLK_ENABLE()
-#define LCD_CS_GPIO_CLK_DISABLE() __HAL_RCC_GPIOH_CLK_DISABLE()
-
-/**
- * @brief  LCD Reset macro definition
- */
-#define LCD_RST_LOW() HAL_GPIO_WritePin(LCD_RST_GPIO_PORT, LCD_RST_PIN, GPIO_PIN_RESET)
-#define LCD_RST_HIGH() HAL_GPIO_WritePin(LCD_RST_GPIO_PORT, LCD_RST_PIN, GPIO_PIN_SET)
-
-/**
- * @brief  LCD Reset pins
- */
-#define LCD_RST_PIN GPIO_PIN_8  /* PC. 08*/
-#define LCD_RST_GPIO_PORT GPIOC /* GPIOC */
-#define LCD_RST_GPIO_CLK_ENABLE() __HAL_RCC_GPIOC_CLK_ENABLE()
-#define LCD_RST_GPIO_CLK_DISABLE() __HAL_RCC_GPIOC_CLK_DISABLE()
-
-/**
- * @brief  LCD Data/Command macro definition
- */
-#define LCD_DC_LOW() HAL_GPIO_WritePin(LCD_DC_GPIO_PORT, LCD_DC_PIN, GPIO_PIN_RESET)
-#define LCD_DC_HIGH() HAL_GPIO_WritePin(LCD_DC_GPIO_PORT, LCD_DC_PIN, GPIO_PIN_SET)
-
-/**
- * @brief
- */
-#define LCD_DC_PIN GPIO_PIN_9  /* PC. 09*/
-#define LCD_DC_GPIO_PORT GPIOC /* GPIOC */
-#define LCD_DC_GPIO_CLK_ENABLE() __HAL_RCC_GPIOC_CLK_ENABLE()
-#define LCD_DC_GPIO_CLK_DISABLE() __HAL_RCC_GPIOC_CLK_DISABLE()
-
-    /** status
+        /** status
      *
      */
     typedef enum
@@ -102,7 +59,7 @@ extern "C"
         user_lcd_status_ERROR_BUS_PROTOCOL_FAILURE = -103,
 
     } user_lcd_status_t;
-    typedef enum 
+    typedef enum
     {
         user_lcd_orientation_landscape = SSD1315_ORIENTATION_LANDSCAPE,
     } user_lcd_orientation_t;
@@ -136,7 +93,7 @@ extern "C"
     /** @defgroup STM32WB5MM_DK_LCD_Exported_Functions Exported Functions
      * @{
      */
-    user_lcd_status_t user_lcd_Init(SSD1315_Drv_t* driver, user_lcd_orientation_t Orientation);
+    user_lcd_status_t user_lcd_Init(SSD1315_Drv_t *driver, user_lcd_orientation_t Orientation);
     user_lcd_status_t user_lcd_DeInit(void);
 
     /* LCD generic APIs: Display control */
